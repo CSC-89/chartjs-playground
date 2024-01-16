6d45092543dc8a9ac57ccff964ac875ef5052724
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-16 17:24:31 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+import { Chart } from "chart.js/auto";
+import { Pie, Line } from "react-chartjs-2";
+import { Data } from "./Data";
+
+function App() {
+  const [data, setData] = useState({
+    labels: Data.map((data) => data.year),
+    datasets: [
+      {
+        labels: Data.map((data) => data.year),
+        data: Data.map((data) => data.cost),
+        backgroundColor: ["green", "pink", "red", "gray", "purple"],
+        hoverOffset: 4,
+      },
+    ],
+    borderColor: "black",
+  });
+
+  return (
+    <>
+      <h1>Charts and graphs</h1>
+
+      <h2>Pie Chart</h2>
+      <div id="pie-chart">
+        <Pie data={data} />
+      </div>
+
+      <h2>Line Chart</h2>
+      <div id="bar-chart">
+      <Line data={data} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,28 @@
+export const Data = [
+    {
+        id: 1,
+        year: 2016,
+        cost: 775
+    },
+    {
+        id: 2,
+        year: 2017,
+        cost: 750
+    },
+    {
+        id: 3,
+        year: 2018,
+        cost: 756
+    },
+    {
+        id: 4,
+        year: 2019,
+        cost: 745
+    },
+
+    {
+        id: 5,
+        year: 2020,
+        cost: 760
+    },
+]
